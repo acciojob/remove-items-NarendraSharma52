d154d038a62351244175649ded8a1ea30879bfc9
Narendra Sharma <154484787+NarendraSharma52@users.noreply.github.com>
2026-02-05 10:54:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// your JS code here. If required.
+
+const select = document.getElementById("colorSelect");
+const button = document.querySelector("input[type='button']");
+
+button.addEventListener("click", () => {
+    const selectedIndex = select.selectedIndex;
+
+    if (selectedIndex !== -1) {
+        select.remove(selectedIndex);
+    }
+});
